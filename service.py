6cf96fb575a2aaf1aacbeba7436d14aac38928c6
tktk92 <87395046+tktk92@users.noreply.github.com>
2026-05-20 12:24:01 +0200

    Keep update service running

--- service.py
+++ service.py
@@ -10,6 +10,7 @@ import xbmcaddon
 ADDON = xbmcaddon.Addon()
 ADDON_PATH = ADDON.getAddonInfo("path")
 LIB_PATH = os.path.join(ADDON_PATH, "resources", "lib")
+UPDATE_CHECK_INTERVAL_SECONDS = 300
 
 if LIB_PATH not in sys.path:
     sys.path.append(LIB_PATH)
@@ -20,14 +21,40 @@ from kodi_library import apply_kodi_library_update_after_addon_update
 from strm import ensure_media_folders
 
 
+def log(message, level=xbmc.LOGINFO):
+    xbmc.log("[IPTV Addon Service] " + str(message), level)
+
+
 class XtreamStrmService(xbmc.Monitor):
+    def run_update_tasks(self):
+        try:
+            ensure_media_folders()
+            skin_updated = apply_arctic_zephyr_reloaded_after_update()
+            library_updated = apply_kodi_library_update_after_addon_update()
+            log("Update-Aufgaben geprueft: Skin={0}, Bibliothek={1}".format(skin_updated, library_updated))
+        except Exception as exc:
+            log("Update-Aufgaben fehlgeschlagen: " + str(exc), xbmc.LOGERROR)
+
+    def run_periodic_tasks(self):
+        try:
+            run_startup_import()
+        except Exception as exc:
+            log("Startup-Import fehlgeschlagen: " + str(exc), xbmc.LOGERROR)
+
     def run(self):
-        if self.waitForAbort(60):
+        log("Service gestartet")
+        if self.waitForAbort(20):
             return
-        ensure_media_folders()
-        apply_arctic_zephyr_reloaded_after_update()
-        apply_kodi_library_update_after_addon_update()
-        run_startup_import()
+
+        self.run_update_tasks()
+        self.run_periodic_tasks()
+
+        while not self.abortRequested():
+            if self.waitForAbort(UPDATE_CHECK_INTERVAL_SECONDS):
+                break
+            self.run_update_tasks()
+
+        log("Service beendet")
 
 
 if __name__ == "__main__":
